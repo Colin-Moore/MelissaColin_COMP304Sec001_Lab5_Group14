8523507169e3ed273f687243e8828faf270f5d63
Colin Moore <cmoore156@gmail.com>
2022-07-26 18:31:28 -0400

    added firebase connection

--- app/src/main/java/com/colin/melissacolin_comp304sec001_lab4_group14/MainActivity.java
+++ app/src/main/java/com/colin/melissacolin_comp304sec001_lab4_group14/MainActivity.java
@@ -1,14 +1,47 @@
 package com.colin.melissacolin_comp304sec001_lab4_group14;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.TextView;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class MainActivity extends AppCompatActivity {
 
+    TextView textview;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        textview = findViewById(R.id.textview);
+        // Write a message to the database
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
+        DatabaseReference myRef = database.getReference("message");
+
+        myRef.setValue("Yo!");
+
+        myRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                // This method is called once with the initial value and again
+                // whenever data at this location is updated.
+                String value = dataSnapshot.getValue(String.class);
+                textview.setText(value);
+            }
+
+            @Override
+            public void onCancelled(DatabaseError error) {
+                // Failed to read value
+                textview.setText(error.toString());
+            }
+        });
     }
+
 }
